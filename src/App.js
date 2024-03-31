2798af2f22a74fbc760cb3a5890fa2a74cb47d57
Piera <pieramblum@gmail.com>
2024-03-31 03:55:39 +0200

    Aufgabe 1 fertig.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>Aufgaben - GUI's realisieren</h2>
       </header>
+      <h3>Aufgabe 1: UI Radiobuttons</h3>
+      <div className="radio">
+        <label>
+          <input 
+            type="radio" 
+            value="blitzEin" 
+            checked={selectedOption === "blitzEin"} 
+            onChange={handleOptionChange} 
+          />
+          Blitz ein
+        </label>
+      </div>
+      <div className="radio">
+        <label>
+          <input 
+            type="radio" 
+            value="blitzAus" 
+            checked={selectedOption === "blitzAus"} 
+            onChange={handleOptionChange} 
+          />
+          Blitz aus
+        </label>
+      </div>
+      <h3>Aufgabe 2: UI Radiobuttons</h3>
     </div>
   );
 }
